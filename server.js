208949bdbe72ea60d2c7e9b6e73b238f2283a016
Madalin Tiutiu <madalin.tiutiu@microfocus.com>
2022-09-02 12:28:25 +0300

    Refactoring

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
-import {config} from "dotenv";
-import fetch from "node-fetch";
+import {getCommits} from "./src/services/BitBucketServerServices.js";
 
-config();
-
-fetch(process.env.BITBUCKET_URL+
-    '/rest/api/1.0/projects/'+process.env.BITBUCKET_PROJECT_NAME+
-    '/repos/'+process.env.BITBUCKET_REPO_SLUG+'/commits/?since='
-    +process.env.SINCE+'&until='+process.env.UNTIL, {
-
-    method: 'GET',
-    headers: {
-        Authorization: "Bearer " + process.env.BITBUCKET_ACCESSTOKEN,
-        'Accept': 'application/json'
-    }
-})
-    .then(response => {
-        console.log(
-            `Response: ${response.status} ${response.statusText}`
-        );
-        return response.text();
-    })
-    .then(text => console.log(text))
-    .catch(err => console.error(err));
-
-
-
-
+const data = await getCommits();
+data.values.forEach(x=>console.log(x.id));
